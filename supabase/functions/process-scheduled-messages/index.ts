2f8117b7dfa327302bb48bcdc666c2bd931f7835
genius70 <buzzmodiio@gmail.com>
2025-08-03 11:59:31 -0500

    Committing all local changes before pulling

--- supabase/functions/process-scheduled-messages/index.ts
+++ supabase/functions/process-scheduled-messages/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.49.8';
 import { serve } from 'https://deno.land/std@0.131.0/http/server.ts';
 
 serve(async () => {
